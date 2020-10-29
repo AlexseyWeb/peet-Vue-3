db61dea31c910610308d72bc17f82f6b29947003
alongenet <alongenet32@gmail.com>
2020-10-30 00:11:11 +0300

    clear default app

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    @{{user.username}} - {{fullName}}
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+
+  data(){
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: '_AlongeNet',
+        firstName: 'Alexsey',
+        lastName: 'Gusakov',
+        email: 'alongenet32@gmail.com',
+        isAdmin: true,
+      },
+    }
+  },
+  computed: {
+    fullName(){
+      return `${this.user.firstName} ${this.user.lastName}`;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
