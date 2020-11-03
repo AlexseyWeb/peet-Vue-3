1b08e979f60f64f1d6fc1bbdf2cdd2847bba9c3e
alongenet <alongenet32@gmail.com>
2020-11-03 15:36:08 +0300

    add photo gallery with dynamic src

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template>
   <div id="app">
-    <h1>Welcome to App</h1>
-      <UserProfile/>
-      <Img/>
+    <h1>Welcome to the photo gallery</h1>
+      <imgCard/>
   </div>
 </template>
 
 <script>
-import UserProfile from './components/UserProfile'
-import Img from './components/Img'
+
+import imgCard from './components/Img'
 
 export default {
   name: 'App',
-  components: { UserProfile, Img}
+  components: {  imgCard},
+ 
 
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'ROBOTO', Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -27,10 +27,13 @@ export default {
   background:rgba(0, 0, 0, 0.637);
   width: 100%;
   height: 100%;
+  
 }
 
 h1 {
-  padding-top: 20px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  text-align: center;
 }
 
 </style>
--- src/components/Img.vue
+++ src/components/Img.vue
@@ -1,7 +1,8 @@
 <template>
-  <img src="../assets/img/city.jpg" alt="City" >
-  <img src="../assets/img/1.jpg" alt="Megapolice">
-  <img src="../assets/img/2.jpg" alt="Night City">
+
+  <div class="images" >
+     <img v-for="img in images" :src="img" :key="img.title"/>
+  </div>
 
 </template>
 
@@ -10,14 +11,38 @@
 <script>
 
 export default {
-   
+    
+   data() {
+      return {
+       images :['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSKTrAGfLGPNDfT0fKyKlSxvPwdRjy7JHbLtg&usqp=CAU',
+       'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTfRHH3uwBsPV-Z5yPXWtc2EHWzgD7FrsjflQ&usqp=CAU',
+       'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTQHsuzlmRErb-IgWk5Jo-37MAjQQbww2J7Xw&usqp=CAU',
+       'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ0ybMdc-AjM05QOZd1y_xlGuCceyJPnaZm6g&usqp=CAU',
+       'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSasFktSE91eckIqEnG5XDAeE61AC_xE-ENgw&usqp=CAU',
+       'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSiuIgsfiWIOUBEaLqnji7DohZIWLrXzzh2dA&usqp=CAU',
+       'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTDxhKKcVzSWx8FrpxcmMZQW2Zi24XYDYXntQ&usqp=CAU',
+       
+       ]
+      } 
+        
+   },
+    
+  
 }
 
 </script>
 
 
 <style scoped>
+
+.images {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+}
     img{
+        margin-top:40px;
         width: 20%;
         height: 30%;
         transition: all 4s ease-in;
@@ -25,12 +50,15 @@ export default {
         box-shadow: 6px 6px 6px rgba(161, 6, 6, 0.774);
         margin-left: 60px;
         border: 4px solid rgba(161, 6, 6, 0.7);
+        justify-content: center;
+        align-items: center;
         
     }
     img:hover{
-        transform: skew(45deg, 30deg);
-        transform: scaleZ(30deg);
+        transform: skew(35deg, 20deg);
+        transform: scaleZ(3deg);
         
     }
+
 </style>>
 
